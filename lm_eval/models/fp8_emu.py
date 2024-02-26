c31498a3312ee17f92d882a3ef2497095e324c68
Dhiraj D Kalamkar <dhiraj.d.kalamkar@intel.com>
2024-02-26 11:31:07 -0800

    Add support for mxfp emulation

--- lm_eval/models/fp8_emu.py
+++ lm_eval/models/fp8_emu.py
@@ -15,9 +15,20 @@ class Fp8EmuLM(HFLM):
         # print(f"Using FP8 emulation - weight_dtype: {weight_dtype}")
         super().__init__(*args, **kwargs)
         if weight_dtype is not None:
-            weight_dtype = get_dtype(weight_dtype)
+            if weight_dtype != "mxfp":
+                weight_dtype = get_dtype(weight_dtype)
+            else:
+                from tpp_pytorch_extension.llm import llm_common
             for n, m in self.model.named_modules():
                 if isinstance(m, torch.nn.Linear) and "lm_head" not in n:
                     print(f"Emulating {weight_dtype} weights for Module {n}")
                     orig_dtype = m.weight.dtype
-                    m.weight.data = m.weight.data.to(weight_dtype).to(orig_dtype)
+                    if weight_dtype != "mxfp":
+                        m.weight.data = m.weight.data.to(weight_dtype).to(orig_dtype)
+                    else:
+                        m.weight.data = llm_common.fused_llm_cpp.mxfp_quant(
+                            m.weight.data
+                        )
+        self.model.to(torch.bfloat16)
+        for n, p in self.model.named_parameters():
+            print(f"{n} - {p.dtype} - {p.shape}")
